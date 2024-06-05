2f35e2a17bdcd4956f968f33e9460495a7c586e8
Obrabigfish <Obrabigfish1@gmail.com>
2024-06-05 19:58:45 +0300

    new

--- hello.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-print("Hello")
-name = input("What is your name")
-name_length = len(name)
-print(name_length)
-if name_length > 5:
-    print(f"Your name has {name_length} chafracters")
-else:
-    print(f"{name_length} Few characters")
---
+++ python/hello.py
@@ -0,0 +1,21 @@
+# #!/usr/bin/python3
+# print("Hello")
+# name = input("What is your name")
+# name_length = len(name)
+# print(name_length)
+# if name_length > 5:
+#     print(f"Your name has {name_length} chafracters")
+# else:
+#     print(f"{name_length} Few characters")
+
+
+# count=0
+# while count < 5:
+#     print(f"count is {count}")
+
+from math import sqrt
+import sys
+result = sqrt(16)
+print(result)
+total = lambda a,b: a * b
+print(2, 4)
